9eabc82208d07f0f7ca4d9745d483f02e7b4843f
Naveen Agarwal <naveen.agarwal@srijan.net>
2021-07-26 09:17:34 +0530

    Initial Commit

---
+++ PlayGround1/PlayGround1App.swift
@@ -0,0 +1,17 @@
+//
+//  PlayGround1App.swift
+//  PlayGround1
+//
+//  Created by Naveen on 26/07/21.
+//
+
+import SwiftUI
+
+@main
+struct PlayGround1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
